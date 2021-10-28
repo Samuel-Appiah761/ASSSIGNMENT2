70f09bbd40c5670ea30004b08f6c886f2cc8abe2
Samuel-Appiah761 <djclassifyd123@gmail.com>
2021-10-28 20:56:56 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    int i;
+    private static String EQUAL = " = ";
+    private static String ONE = " 1 ";
+    private static String SIGN = " + ";
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(EQUAL + i);
+
+        }
+
+
+    }
+}
+
